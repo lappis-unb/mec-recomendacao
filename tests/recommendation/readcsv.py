ecee3f24aa360b840305d50683f76d4d4164d9ff
Yudi Yamane <yudiyamane@gmail.com>
2022-12-02 23:53:24 -0300

    refactor: move valores escalares de azul para arquivos

--- tests/recommendation/readcsv.py
+++ tests/recommendation/readcsv.py
@@ -3,6 +3,7 @@ import json
 from os.path import join
 from pandas import DataFrame
 
+from recommendation.tariff import Tariff
 from recommendation.green import GreenPercentileCalculator, GreenTariff
 from recommendation.blue import BluePercentileCalculator, BlueTariff
 from recommendation import ContractRecommendationCalculator
@@ -31,7 +32,10 @@ class CsvData:
     green_tariff: GreenTariff
     expected_recommended_tariff_flag: str
     expected_blue_percentiles: dict[str, DataFrame]
+    expected_blue_percentiles_total_in_reais: dict[str, float]
+    expected_green_percentiles_total_in_reais: dict[str, float]
     expected_green_percentiles: dict[str, DataFrame]
+    expected_summary_scalar_values: dict[str, dict]
     expected_blue_summary: DataFrame
     expected_green_summary: DataFrame
     expected_current_contract: DataFrame
@@ -46,25 +50,34 @@ class CsvReader:
         data = CsvData()
         data.consumption_history = self._read_consumption_history(HISTORY_HEADERS)
         data.expected_blue_percentiles = self._read_expected_percentiles('blue_per_%s.csv', B_PERCENTILES, B_PERCENTILE_HEADERS)
+        expected_percentiles_total_in_reais = self._read_expected_percentiles_total_in_reais()
+        data.expected_blue_percentiles_total_in_reais = expected_percentiles_total_in_reais['blue']
+        data.expected_green_percentiles_total_in_reais = expected_percentiles_total_in_reais['green']
         data.expected_green_percentiles = self._read_expected_percentiles('green_per_%s.csv', G_PERCENTILES, G_PERCENTILE_HEADERS)
+        data.expected_summary_scalar_values = self._read_expected_summary_scalar_values()
         data.expected_blue_summary = self._read_expected_summary('blue', B_SUMMARY_HEADERS)
         data.expected_green_summary = self._read_expected_summary('green', G_SUMMARY_HEADERS)
         data.expected_recommendation = pd.read_csv(join(self.path, 'recommendation.csv'), names=RECOMENDATION_HEADERS, sep=SEP)
         data.expected_current_contract = pd.read_csv(join(self.path, 'current_contract.csv'), names=CURRENT_CONTRACT_HEADERS, sep=SEP)
-        fj = open(join(self.path, 'tariff.json'))
-        tariff = json.load(fj)
+
+        tariff = self._read_tariff()
         data.blue_tariff = BlueTariff(**tariff['blue'])
         data.green_tariff = GreenTariff(**tariff['green'])
         data.expected_recommended_tariff_flag = tariff['recommended_flag']
         data.current_tariff_flag = tariff['current_flag']
-        fj.close()
         return data
+    
+    def _read_expected_percentiles_total_in_reais(self):
+        f = open(join(self.path, 'per_total_in_reais.json'), 'r')
+        total_in_reais  = json.load(f)
+        f.close()
+        return {'blue': {**total_in_reais['blue']}, 'green': {**total_in_reais['green']}}
 
     def _read_consumption_history(self, headers: list[str]) -> DataFrame:
         return pd.read_csv(join(self.path, 'consumption.csv'), sep=SEP, names=headers)
 
 
-    def _read_expected_percentiles(self, 
+    def _read_expected_percentiles(self,
         filename_template: str,
         percentiles: list[float],
         percentile_headers: list[str]
@@ -74,17 +87,28 @@ class CsvReader:
             p_str = str(p)
             frame = pd.read_csv(
                 join(self.path, filename_template % p_str),
-                sep='\t',
+                sep=SEP,
                 names=percentile_headers)
             expected_percentiles[p_str] = frame
         return expected_percentiles
 
+    def _read_expected_summary_scalar_values(self) -> dict:
+        f = open(join(self.path, 'summary_scalar_values.json'), 'r')
+        scalar_values = json.load(f)
+        f.close()
+        return scalar_values
 
-    def _read_expected_summary(self, color: str, summary_headers: list[str]) -> DataFrame:
+    def _read_expected_summary(self, flag: str, summary_headers: list[str]) -> DataFrame:
         return pd.read_csv(
-            join(self.path, f'{color}_per_summary.csv'), 
-            sep='\t', 
+            join(self.path, f'{flag}_per_summary.csv'), 
+            sep=SEP,
             names=summary_headers)
+    
+    def _read_tariff(self):
+        f = open(join(self.path, 'tariff.json'), 'r')
+        tariff = json.load(f)
+        f.close()
+        return tariff
 
 
 # Os casos de teste estão em recommendation/tests/data/uc_{id}/**
--- tests/recommendation/test_blue_percentile_calculator.py
+++ tests/recommendation/test_blue_percentile_calculator.py
@@ -1,4 +1,4 @@
-from pandas import DataFrame
+from tests.recommendation.readcsv import test_cases
 from pandas.testing import assert_frame_equal
 import pytest
 from pytest import approx
@@ -7,19 +7,11 @@ from recommendation.blue import BluePercentileCalculator
 
 
 PERCENTILES = BluePercentileCalculator.PERCENTILES
-PERCENTILE_HEADERS = BluePercentileCalculator.PERCENTILE_HEADERS
-SUMMARY_HEADERS = BluePercentileCalculator.SUMMARY_HEADERS
-
 ABSOLUTE_TOLERANCE = 0.01
-
-from tests.recommendation.readcsv import test_cases
-
 test_data = [(_id) for _id in test_cases.keys()]
-
 
 @pytest.mark.parametrize('_id', test_data)
 def test_blue_per_calculator(_id: str):
-    # setup
     data = test_cases[_id]
     consumption_history = data.consumption_history
     sut = BluePercentileCalculator(consumption_history, data.blue_tariff)
@@ -29,12 +21,15 @@ def test_blue_per_calculator(_id: str):
     for p in PERCENTILES:
         p_str = str(p)
         assert_frame_equal(
-            # Desconsidera a coluna total_in_reais,
-            # que é testada nas asserções seguintes:
-            result.percentiles[p_str].iloc[:, 0:-2],
-            data.expected_blue_percentiles[p_str].iloc[:, 0:-2],
+            # TODO: Desconsidera a coluna "total_in_reais",
+            # que é testada na asserção seguinte. "total_in_reais" nem deveria
+            # ser uma coluna. Deveria ser um escalar
+            result.percentiles[p_str].drop('total_in_reais', axis=1),
+            data.expected_blue_percentiles[p_str].drop('total_in_reais', axis=1),
             check_exact=False,
             atol=ABSOLUTE_TOLERANCE)
+
+        assert approx(data.expected_blue_percentiles_total_in_reais[p_str], abs=ABSOLUTE_TOLERANCE) == result.percentiles[p_str].total_in_reais
 
     # teste resumo
     assert_frame_equal(
@@ -42,52 +37,9 @@ def test_blue_per_calculator(_id: str):
         data.expected_blue_summary,
         check_exact=False,
         atol=ABSOLUTE_TOLERANCE)
-
-
-
-class TestBluePercentileCalculator:
-    consumption_history: DataFrame
-    sut: BluePercentileCalculator
-
-    def setup_class(self):
-        self.consumption_history = test_cases['1011101-5'].consumption_history
-        self.expected_percentiles = test_cases['1011101-5'].expected_blue_percentiles
-        self.expected_summary = test_cases['1011101-5'].expected_blue_summary
-        self.sut = BluePercentileCalculator(self.consumption_history, test_cases['1011101-5'].blue_tariff)
-        self.result = self.sut.calculate()
-
-    def test_calculates_percentiles(self):
-        for p in PERCENTILES:
-            p_str = str(p)
-            assert_frame_equal(
-                # Desconsidera a coluna total_in_reais,
-                # que é testada nas asserções seguintes:
-                self.result.percentiles[p_str].iloc[:, 0:-2],
-                self.expected_percentiles[p_str].iloc[:, 0:-2],
-                check_exact=False,
-                atol=ABSOLUTE_TOLERANCE)
-
-        assert approx(695755.82) == self.result.percentiles['0.1'].total_in_reais
-        assert approx(656298.46) == self.result.percentiles['0.2'].total_in_reais
-        assert approx(591048.63) == self.result.percentiles['0.3'].total_in_reais
-        assert approx(563043.34) == self.result.percentiles['0.4'].total_in_reais
-        assert approx(527318.02) == self.result.percentiles['0.5'].total_in_reais
-        assert approx(515118.23) == self.result.percentiles['0.6'].total_in_reais
-        assert approx(514027.84) == self.result.percentiles['0.7'].total_in_reais
-        assert approx(516997.98) == self.result.percentiles['0.8'].total_in_reais
-        assert approx(522351.99) == self.result.percentiles['0.9'].total_in_reais
-        assert approx(534204.22) == self.result.percentiles['0.95'].total_in_reais
-        assert approx(542520.09) == self.result.percentiles['0.98'].total_in_reais
-
-    def test_calculates_summary(self):
-        assert_frame_equal(
-            self.result.summary,
-            self.expected_summary,
-            check_exact=False,
-            atol=ABSOLUTE_TOLERANCE)
-
-        assert approx(311.0) == self.result.summary.peak_demand_in_kw
-        assert approx(467.0) == self.result.summary.off_peak_demand_in_kw
-        assert approx(469740.28) == self.result.summary.total_consumption_value_in_reais
-        assert approx(514027.84) == self.result.summary.smallest_total_demand_value_in_reais
-        assert approx(983768.11) == self.result.summary.total_total_value_in_reais
+    
+    assert approx(data.expected_summary_scalar_values['blue']['peak_demand_in_kw'], abs=ABSOLUTE_TOLERANCE) == result.summary.peak_demand_in_kw
+    assert approx(data.expected_summary_scalar_values['blue']['off_peak_demand_in_kw'], abs=ABSOLUTE_TOLERANCE) == result.summary.off_peak_demand_in_kw
+    assert approx(data.expected_summary_scalar_values['blue']['total_consumption_value_in_reais'], abs=ABSOLUTE_TOLERANCE) == result.summary.total_consumption_value_in_reais
+    assert approx(data.expected_summary_scalar_values['blue']['smallest_total_demand_value_in_reais'], abs=ABSOLUTE_TOLERANCE) == result.summary.smallest_total_demand_value_in_reais
+    assert approx(data.expected_summary_scalar_values['blue']['total_total_value_in_reais'], abs=ABSOLUTE_TOLERANCE) == result.summary.total_total_value_in_reais
